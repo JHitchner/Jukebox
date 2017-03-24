128b01541e4905bbb7ed7c6cbad5f2e85714ddbb
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-03-24 15:46:53 -0400

    play/pause/previous/next buttons work but little buggy

--- hitchner_jukebox.js
+++ hitchner_jukebox.js
@@ -4,77 +4,78 @@ var pauseBtn = document.querySelector("#pause");
 var backBtn = document.querySelector("#back");
 var nextBtn = document.querySelector("#next");
 
-var audio = document.querySelector("#audio");
+var audio = document.querySelectorAll(".audio");
+
+console.log(audio.length);
+
 
 function Jukebox() {
-  this.list = [];
+  // this.list = [];
   this.index = 0;
-  this.currentSong = audio[this.index]
-  // this.list[this.index]
-//    // the code for what happens when you create a Jukebox object
-//    //  goes here
+  this.currentSong = audio[this.index];
  }
 
  // defines the Jukebox prototype object
- 
+
 Jukebox.prototype.start = function () {
-  this.currentSong = this.list[this.index];
-  console.log(currentSong)
-  console.log("I am playing")
+  this.currentSong = audio[this.index];
+  console.log(this.currentSong);
+  this.currentSong.play();
 }
 
 Jukebox.prototype.stop = function () {
-// code for stopping song here
-  console.log("I am paused")
+  this.currentSong.pause();
  };
 
 Jukebox.prototype.back = function () {
+   this.currentSong.pause();
+   this.currentSong.currentTime = 0;
 // code for moving back a song here
-  this.currentSong = this.list[this.index - 1];
+  // for( var i = 0; i < this.currentSong.length; i++)
   if(this.index - 1 < 0){
-    this.index = this.list.length - 1
-    console.log(this.list[this.index]);
-}else if( this.index - 1 >= 0){
-    console.log(this.currentSong)
-    this.index = this.index - 1
+    this.index = audio.length - 1;
+    this.currentSong.play();
+  }else{
+    this.currentSong = audio[this.index - 1];
+    this.currentSong.play();
+    this.index--;
   };
 }
 Jukebox.prototype.next = function () {
-  this.currentSong = this.list[this.index + 1]
-  if(this.index + 1 >= this.list.length){
-    this.index = 0;
-    console.log(this.list[this.index])
-  }else if (this.index + 1 < this.list.length){
-    console.log(this.currentSong)
-    this.index++
+  this.currentSong.pause();
+  this.currentSong.currentTime = 0;
+  this.currentSong = audio[this.index + 1]
+  if(this.index + 1 >= audio.length){
+    this.currentSong = audio[0]
+    this.currentSong.play();
+  }else{
+    this.index++;
+    this.currentSong.play();
   }
 };
 
- // the rest of your prototype methods would go here
-
- // creates a new Jukebox object
- // this variable has global scope after this point
- var jukebox = new Jukebox();
+var jukebox = new Jukebox();
 
 
  // adds an event listener for when you click the play button
 playBtn.addEventListener("click", function(event){
-   audio.play()
-   console.log("i was clicked")
+   jukebox.start();
+   console.log("i was clicked");
   //  jukebox.play(this.index);
-   // prevents link from going to the next page
    event.preventDefault()
 })
-    //
-  pauseBtn.addEventListener("click", function(event){
-     audio.pause();
-     event.preventDefault()
-  })
-  backBtn.addEventListener("click", function(event){
-     jukebox.back(this.index);
-     event.preventDefault()
-  })
-  nextBtn.addEventListener("click", function(event){
-     jukebox.next();
-     event.preventDefault()
-  })
+pauseBtn.addEventListener("click", function(event){
+  jukebox.stop();
+  event.preventDefault();
+  console.log("i was clicked_pause");
+})
+backBtn.addEventListener("click", function(event){
+  jukebox.back();
+  console.log("I was clicked_back");
+  event.preventDefault();
+})
+nextBtn.addEventListener("click", function(event){
+  jukebox.next();
+  console.log("I was clicked_next");
+  event.preventDefault();
+})
