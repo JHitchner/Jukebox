670a6713304547b42af6988feb9e1fd22f6628d6
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-03-25 16:07:45 -0400

    working next and back buttons :)

--- hitchner_jukebox.js
+++ hitchner_jukebox.js
@@ -1,56 +1,72 @@
 
+// show and search songs in html form
+// fix the button next and previous
+// create a stop button
 var playBtn = document.querySelector("#play");
 var pauseBtn = document.querySelector("#pause");
+var stopBtn = document.querySelector("#stop");
 var backBtn = document.querySelector("#back");
 var nextBtn = document.querySelector("#next");
-
+var titles = document.querySelector("#songTitle")
 var audio = document.querySelectorAll(".audio");
 
 console.log(audio.length);
 
+var songNames =["Fan Song", "Dragonborn", "Thunderhorse"];
+
 
 function Jukebox() {
-  // this.list = [];
   this.index = 0;
   this.currentSong = audio[this.index];
+  this.title = songNames[this.index];
  }
-
  // defines the Jukebox prototype object
 
 Jukebox.prototype.start = function () {
-  this.currentSong = audio[this.index];
-  console.log(this.currentSong);
   this.currentSong.play();
+  console.log(this.currentSong)
+  console.log(songNames[this.index])
 }
-
-Jukebox.prototype.stop = function () {
+Jukebox.prototype.pause = function () {
   this.currentSong.pause();
- };
+};
+Jukebox.prototype.stop = function (){
+  this.currentSong.pause();
+  this.currentSong.currentTime = 0;
+};
 
 Jukebox.prototype.back = function () {
-   this.currentSong.pause();
-   this.currentSong.currentTime = 0;
-// code for moving back a song here
-  // for( var i = 0; i < this.currentSong.length; i++)
-  if(this.index - 1 < 0){
-    this.index = audio.length - 1;
-    this.currentSong.play();
-  }else{
-    this.currentSong = audio[this.index - 1];
-    this.currentSong.play();
+  this.currentSong.pause();
+  this.currentSong.currentTime = 0;
+  if (this.index - 1 >= 0){
     this.index--;
-  };
+    this.currentSong = audio[this.index];
+    this.currentSong.play();
+    console.log(audio[this.index]);
+    console.log(songNames[this.index]);
+  }else if(this.index - 1 < 0){
+      this.index = audio.length - 1
+      this.currentSong = audio[this.index];
+      this.currentSong.play();
+      console.log(audio[this.index]);
+      console.log(songNames[this.index]);
+    }
 }
 Jukebox.prototype.next = function () {
   this.currentSong.pause();
   this.currentSong.currentTime = 0;
-  this.currentSong = audio[this.index + 1]
-  if(this.index + 1 >= audio.length){
-    this.currentSong = audio[0]
-    this.currentSong.play();
-  }else{
+  if (this.index + 1 < audio.length){
     this.index++;
+    this.currentSong = audio[this.index];
     this.currentSong.play();
+    console.log(audio[this.index])
+    console.log(songNames[this.index]);
+  }else if(this.index + 1 == audio.length){
+    this.index = 0;
+    this.currentSong = audio[this.index]
+    this.currentSong.play();
+    console.log(audio[this.index]);
+    console.log(songNames[this.index]);
   }
 };
 
@@ -64,18 +80,23 @@ playBtn.addEventListener("click", function(event){
   //  jukebox.play(this.index);
    event.preventDefault()
 })
-pauseBtn.addEventListener("click", function(event){
+stopBtn.addEventListener("click", function(event){
   jukebox.stop();
   event.preventDefault();
-  console.log("i was clicked_pause");
+  console.log("i was clicked stop")
+})
+pauseBtn.addEventListener("click", function(event){
+  jukebox.pause();
+  event.preventDefault();
+  console.log("i was clicked pause");
 })
 backBtn.addEventListener("click", function(event){
   jukebox.back();
-  console.log("I was clicked_back");
+  console.log("i was clicked_back");
   event.preventDefault();
 })
 nextBtn.addEventListener("click", function(event){
   jukebox.next();
-  console.log("I was clicked_next");
+  console.log("i was clicked next");
   event.preventDefault();
 })
