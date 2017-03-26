e974fadf48478ab0ddd63098d2a60f09677371cb
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-03-25 20:27:09 -0400

    my totally working jukebox

--- hitchner_jukebox.js
+++ hitchner_jukebox.js
@@ -1,7 +1,5 @@
 
 // show and search songs in html form
-// fix the button next and previous
-// create a stop button
 var playBtn = document.querySelector("#play");
 var pauseBtn = document.querySelector("#pause");
 var stopBtn = document.querySelector("#stop");
@@ -9,24 +7,22 @@ var backBtn = document.querySelector("#back");
 var nextBtn = document.querySelector("#next");
 var titles = document.querySelector("#songTitle")
 var audio = document.querySelectorAll(".audio");
+// var photos = document.querySelectorAll(".covers");
 
-console.log(audio.length);
-
-var songNames =["Fan Song", "Dragonborn", "Thunderhorse"];
-
+var songNames =["FanSong: Dethklok", "Dragonborn: Jeremy Soule", "Thunderhorse: Dethklok", "Awaken: Dethklok"];
 
 function Jukebox() {
   this.index = 0;
   this.currentSong = audio[this.index];
   this.title = songNames[this.index];
- }
+  // this.image = photos[this.index];
+};
  // defines the Jukebox prototype object
 
 Jukebox.prototype.start = function () {
   this.currentSong.play();
-  console.log(this.currentSong)
-  console.log(songNames[this.index])
-}
+  titles.innerHTML = songNames[this.index];
+};
 Jukebox.prototype.pause = function () {
   this.currentSong.pause();
 };
@@ -42,16 +38,14 @@ Jukebox.prototype.back = function () {
     this.index--;
     this.currentSong = audio[this.index];
     this.currentSong.play();
-    console.log(audio[this.index]);
-    console.log(songNames[this.index]);
+    titles.innerHTML = songNames[this.index];
   }else if(this.index - 1 < 0){
-      this.index = audio.length - 1
-      this.currentSong = audio[this.index];
-      this.currentSong.play();
-      console.log(audio[this.index]);
-      console.log(songNames[this.index]);
-    }
-}
+    this.index = audio.length - 1
+    this.currentSong = audio[this.index];
+    this.currentSong.play();
+    titles.innerHTML = songNames[this.index];
+  };
+};
 Jukebox.prototype.next = function () {
   this.currentSong.pause();
   this.currentSong.currentTime = 0;
@@ -59,44 +53,50 @@ Jukebox.prototype.next = function () {
     this.index++;
     this.currentSong = audio[this.index];
     this.currentSong.play();
-    console.log(audio[this.index])
-    console.log(songNames[this.index]);
+    titles.innerHTML = songNames[this.index];
   }else if(this.index + 1 == audio.length){
     this.index = 0;
     this.currentSong = audio[this.index]
     this.currentSong.play();
     console.log(audio[this.index]);
-    console.log(songNames[this.index]);
-  }
+    titles.innerHTML = songNames[this.index];
+  };
 };
 
 var jukebox = new Jukebox();
 
-
- // adds an event listener for when you click the play button
 playBtn.addEventListener("click", function(event){
-   jukebox.start();
-   console.log("i was clicked");
-  //  jukebox.play(this.index);
-   event.preventDefault()
-})
+  playBtn.style.display = "none";
+  pauseBtn.style.display = "block";
+  jukebox.start();
+  // console.log("i was clicked");
+  event.preventDefault();
+});
 stopBtn.addEventListener("click", function(event){
   jukebox.stop();
+  playBtn.style.display = "block";
+  pauseBtn.style.display = "none";
   event.preventDefault();
-  console.log("i was clicked stop")
-})
+  // console.log("i was clicked stop")
+});
 pauseBtn.addEventListener("click", function(event){
+  playBtn.style.display = "block";
+  pauseBtn.style.display = "none";
   jukebox.pause();
   event.preventDefault();
-  console.log("i was clicked pause");
-})
+  // console.log("i was clicked pause");
+});
 backBtn.addEventListener("click", function(event){
+  playBtn.style.display = "none";
+  pauseBtn.style.display = "block";
   jukebox.back();
-  console.log("i was clicked_back");
+  // console.log("i was clicked back");
   event.preventDefault();
-})
+});
 nextBtn.addEventListener("click", function(event){
+  playBtn.style.display = "none";
+  pauseBtn.style.display = "block";
   jukebox.next();
-  console.log("i was clicked next");
+  // console.log("i was clicked next");
   event.preventDefault();
-})
+});
