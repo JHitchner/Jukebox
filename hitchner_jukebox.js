28cd32dd79f55f0e8269775272c263ba911d7e64
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-03-26 15:04:25 -0400

    final touches for Sunday assignment

--- hitchner_jukebox.js
+++ hitchner_jukebox.js
@@ -1,102 +1,109 @@
 
-// show and search songs in html form
+// My Buttons
 var playBtn = document.querySelector("#play");
 var pauseBtn = document.querySelector("#pause");
 var stopBtn = document.querySelector("#stop");
 var backBtn = document.querySelector("#back");
 var nextBtn = document.querySelector("#next");
+
+// My song array and song names
 var titles = document.querySelector("#songTitle")
 var audio = document.querySelectorAll(".audio");
-// var photos = document.querySelectorAll(".covers");
-
-var songNames =["FanSong: Dethklok", "Dragonborn: Jeremy Soule", "Thunderhorse: Dethklok", "Awaken: Dethklok"];
+var songNames =["FanSong: Dethklok", "Thunderhorse: Dethklok", "Awaken: Dethklok"];
 
 function Jukebox() {
   this.index = 0;
   this.currentSong = audio[this.index];
   this.title = songNames[this.index];
-  // this.image = photos[this.index];
 };
- // defines the Jukebox prototype object
-
+// Play function
 Jukebox.prototype.start = function () {
   this.currentSong.play();
   titles.innerHTML = songNames[this.index];
 };
+// Pause function
 Jukebox.prototype.pause = function () {
   this.currentSong.pause();
 };
-Jukebox.prototype.stop = function (){
+// Stop: Pause and reset song time to beginning
+Jukebox.prototype.stop = function() {
   this.currentSong.pause();
   this.currentSong.currentTime = 0;
 };
-
-Jukebox.prototype.back = function () {
+// Go back one song
+Jukebox.prototype.back = function() {
   this.currentSong.pause();
   this.currentSong.currentTime = 0;
-  if (this.index - 1 >= 0){
+// If index - 1 is greater than zero then move down array
+// by one. Play that song and set the innerHTML to that song.
+  if (this.index - 1 >= 0) {
     this.index--;
     this.currentSong = audio[this.index];
     this.currentSong.play();
     titles.innerHTML = songNames[this.index];
-  }else if(this.index - 1 < 0){
+// If index - 1 is less than 0 go to the end of the array
+// Play that song and set innerHTML to that song.
+  }else if(this.index - 1 < 0) {
     this.index = audio.length - 1
     this.currentSong = audio[this.index];
     this.currentSong.play();
     titles.innerHTML = songNames[this.index];
   };
 };
-Jukebox.prototype.next = function () {
+// Go forward one song
+Jukebox.prototype.next = function() {
   this.currentSong.pause();
   this.currentSong.currentTime = 0;
-  if (this.index + 1 < audio.length){
+// If index + 1 is less than the length of my array move up
+// by one. Play that song and set innerHTML to that song.
+  if (this.index + 1 < audio.length) {
     this.index++;
     this.currentSong = audio[this.index];
     this.currentSong.play();
     titles.innerHTML = songNames[this.index];
-  }else if(this.index + 1 == audio.length){
+// If index + 1 equals audio length go to beginning of array.
+  }else if(this.index + 1 == audio.length) {
     this.index = 0;
     this.currentSong = audio[this.index]
     this.currentSong.play();
-    console.log(audio[this.index]);
     titles.innerHTML = songNames[this.index];
   };
 };
 
 var jukebox = new Jukebox();
-
-playBtn.addEventListener("click", function(event){
+// Function to hide play button when pause is clicked
+function hidePlay() {
   playBtn.style.display = "none";
   pauseBtn.style.display = "block";
+};
+// Function to hide pause button when play is clicked
+function hidePause() {
+  playBtn.style.display = "block";
+  pauseBtn.style.display = "none";
+};
+
+playBtn.addEventListener("click", function(event) {
+  hidePlay();
   jukebox.start();
-  // console.log("i was clicked");
   event.preventDefault();
 });
-stopBtn.addEventListener("click", function(event){
+stopBtn.addEventListener("click", function(event) {
   jukebox.stop();
-  playBtn.style.display = "block";
-  pauseBtn.style.display = "none";
+  hidePause();
   event.preventDefault();
-  // console.log("i was clicked stop")
 });
-pauseBtn.addEventListener("click", function(event){
-  playBtn.style.display = "block";
-  pauseBtn.style.display = "none";
+pauseBtn.addEventListener("click", function(event) {
+  hidePause();
   jukebox.pause();
   event.preventDefault();
-  // console.log("i was clicked pause");
 });
-backBtn.addEventListener("click", function(event){
-  playBtn.style.display = "none";
-  pauseBtn.style.display = "block";
+backBtn.addEventListener("click", function(event) {
+  hidePlay();
   jukebox.back();
-  // console.log("i was clicked back");
   event.preventDefault();
 });
 nextBtn.addEventListener("click", function(event){
-  playBtn.style.display = "none";
-  pauseBtn.style.display = "block";
+  hidePlay();
   jukebox.next();
-  // console.log("i was clicked next");
   event.preventDefault();
 });
